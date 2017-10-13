a77e5543fdc6c5a30227f8f78b6413e09665247f
Todd Matthews <gtmatthews@gmail.com>
2017-10-13 16:40:13 -0400

    Organize stories and components within <story-board>

---
+++ src/components/storyCard.vue
@@ -0,0 +1,50 @@
+<template>
+    <div class="card">
+        <p>{{story.name}}</p>
+      </a>
+    </div>
+</template>
+
+<script>
+
+export default {
+  name: 'storyCard',
+
+  props: ['story']
+
+}; // export
+</script>
+
+<style scoped>
+a {
+  padding: 0;
+  text-decoration: none;
+  color: #333;
+}
+
+a:hover {
+  text-decoration: underline;
+  color: #669bbc;
+  /* cursor: pointer; */
+}
+
+.bug {
+  border-left: 4px solid #F25F5C;
+}
+
+.feature {
+  border-left: 4px solid #FFE066;
+}
+
+.chore {
+  border-left: 4px solid #669bbc;
+}
+
+.card {
+  background: white;
+  margin-bottom: 10px;
+  box-shadow: 1px 1px 2px #888;
+  overflow: auto;
+  position: relative;
+}
+</style>
